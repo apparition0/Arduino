b8da9a58a80100b476072f2e87b3f5a5f3f11fba
Philip Wheatley <apparition0@verizon.net>
2014-11-30 16:38:17 -0600

    Starts sim908 too!

--- CarGps7/Console.ino
+++ CarGps7/Console.ino
@@ -4,7 +4,12 @@ void Console::init()
 {
   Serial.begin(9600);
 }
+
 void Console::print(char *str)
 {
   Serial.print(str);
+}
+void Console::print(char c)
+{
+  Serial.print(c);
 }
--- CarGps7/Gsm.ino
+++ CarGps7/Gsm.ino
@@ -9,23 +9,34 @@ void Gsm::init()
   mm = millis();
   Serial2.begin(9600);
   Console::print(sendATcommand("ATZ","OK",to));
+  Console::print(sendATcommand("AT+CGPSPWR=1","OK",to));
+  Console::print(sendATcommand("AT+CGPSOUT=51","OK",to));
+  Console::print(sendATcommand("AT+CGPSRST=1","OK",to)); 
 }
 
 char *Gsm::sendATcommand(char *cmd,char *ans, long int timeout)
 {
   int i=0;
+  bool answer = false;
+  long int start = millis();
   memset(resp,0,ARRAYSIZE);
-  while(Serial2.available()>0) Serial2.read();
+  while(Serial2.available()>0) Console::print(Serial2.read());
   Serial2.println(cmd);
-  
-  while(Serial2.available() != 0)
+  Console::print("-");
+  do 
+  {
+    Console::print(".");
+    if(Serial2.available() != 0)
     {
        resp[i] = Serial2.read();
+       Console::print(resp);
        i++;
        if(strstr(resp,ans) != NULL)
-         break;
+         answer = true;
     }
-    return resp;
+  } while ( (!answer) && (millis() < (start+timeout) ) );
+  Console::print("+");
+  return resp;
 }
 
 void Gsm::transmit(char *str)
